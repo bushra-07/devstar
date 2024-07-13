cbda3c87b024ec376d7c8b42498b09b89c2da507
Saiesh Gaonkar <sng.prg.cod.2022@gmail.com>
2024-07-13 22:33:24 +0530

    removed the unwanted commits and changes made by me

--- src/routes/(tools)/css-toolkit/+page.svelte
+++ src/routes/(tools)/css-toolkit/+page.svelte
@@ -1,70 +1,27 @@
 <script>
-  import TextGradient from './TextGradient.svelte';
-  import BoxShadow from './BoxShadow.svelte';
-  import HoverEffect from './HoverEffect.svelte';
- 
+  import { Tabs, TabItem } from "flowbite-svelte";
+import Csstoolkitwave from "$lib/csstoolkitwave.svelte";
+import Csstoolkitstripes from "$lib/csstoolkitstripes.svelte";
+import Csstoolkitgradient from "$lib/csstoolkitgradient.svelte";
+//   let textColor = "#000000"; // Default color
 
-  let currentTool = 'TextGradient';
+//   function handleColorChange(event) {
+//     textColor = event.target.value;
+//   }
+
+
 </script>
 
-<style>
-  .container {
-    display: flex;
-    height: auto;
-    /* height: 100vh; */
-  }
 
-  .sidebar {
-    width: 200px;
-    background-color: #f0f0f0;
-    padding: 20px;
-  }
-
-  .content {
-    flex-grow: 1;
-    padding: 20px;
-    overflow-y: auto;
-  }
-
-  .sidebar button {
-    display: block;
-    width: 100%;
-    padding: 10px;
-    margin-bottom: 10px;
-    border: none;
-    background-color: #007bff;
-    color: white;
-    cursor: pointer;
-    border-radius: 4px;
-    text-align: left;
-  }
-
-  .sidebar button.active {
-    background-color: #0056b3;
-  }
-</style>
-
-<div class="container">
-  <div class="sidebar">
-    <button class:active={currentTool === 'TextGradient'} on:click={() => currentTool = 'TextGradient'}>
-      CSS Text Gradient Generator
-    </button>
-    <button class:active={currentTool === 'BoxShadow'} on:click={() => currentTool = 'BoxShadow'}>
-      CSS Box Shadow Generator
-    </button>
-    <button class:active={currentTool === 'HoverEffect'} on:click={() => currentTool = 'HoverEffect'}>
-      CSS Hover Effect Generator
-    </button>
-  
-  </div>
-
-  <div class="content">
-    {#if currentTool === 'TextGradient'}
-      <TextGradient />
-    {:else if currentTool === 'BoxShadow'}
-      <BoxShadow />
-    {:else if currentTool === 'HoverEffect'}
-      <HoverEffect />
-    {/if}
-  </div>
+<div class="overflow-auto max-w-[1200px]">
+<Tabs>
+  <TabItem open title="wave">
+    <Csstoolkitwave></Csstoolkitwave>
+  </TabItem>
+  <TabItem title="Stripes">
+    <Csstoolkitstripes></Csstoolkitstripes>
+  </TabItem>
+  <TabItem title="gradient"><Csstoolkitgradient></Csstoolkitgradient></TabItem
+  >
+</Tabs>
 </div>
